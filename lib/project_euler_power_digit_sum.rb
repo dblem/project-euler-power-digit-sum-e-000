3e8d13c2a49b6fcbf50c17596bc8d697305f2f71
dblem <dax.blem@gmail.com>
2016-02-27 19:05:26 +0000

    Done.

--- lib/project_euler_power_digit_sum.rb
+++ lib/project_euler_power_digit_sum.rb
@@ -1,1 +1,9 @@
-# code your solution here
+def power_digit_sum(x, y)
+  sum = 0
+  power = (x ** y).to_s
+  power_digit_array = power.split("")
+  power_digit_array.each do |z|
+    sum += z.to_i
+  end
+  sum
+end
